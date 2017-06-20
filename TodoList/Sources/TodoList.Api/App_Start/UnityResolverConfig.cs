1b9f115016ab151f8f766d0efa7e51cfe16dff0c
Vlastimil Martinek <VlastimilM@kentico.com>
2017-06-20 15:30:44 +0200

    KA-131 - Move projects to folders Sources and Tests

---
+++ TodoList/Sources/TodoList.Api/App_Start/UnityResolverConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Microsoft.Practices.Unity;
+using TodoList.Api.Resolvers;
+using TodoList.Contracts.Interfaces;
+using TodoList.Repositories;
+
+namespace TodoList.Api
+{
+    internal static class UnityResolverConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var container = new UnityContainer()
+                .RegisterTypes<RepositoryBootstrapper>()
+                .RegisterTypes<ApiBootstrapper>();
+                
+            config.DependencyResolver = new UnityResolver(container);
+        }
+
+        private static IUnityContainer RegisterTypes<TBootstrapper>(this IUnityContainer container)
+            where TBootstrapper : IBootstrapper, new()
+            => new TBootstrapper().Register(container);
+    }
+}
---
+++ TodoList/Tests/TodoList.Api.Tests/Helpers/ListItemComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TodoList.Contracts.Models;
+
+namespace TodoList.Api.Tests.Helpers
+{
+    internal class ListItemComparer : IEqualityComparer<ListItem>
+    {
+        public bool Equals(ListItem x, ListItem y)
+        {
+            return x.Id.Equals(y.Id) && x.Text.Equals(y.Text);
+        }
+
+        public int GetHashCode(ListItem obj)
+        {
+            return (obj.Id.GetHashCode() * 397) ^ (obj.Text?.GetHashCode() ?? 0);
+        }
+    }
+}
