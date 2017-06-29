cf203faa744083914bc980fb86f05b88bbf4ad0a
Linda Hanslikova <LindaH@kentico.com>
2017-06-29 12:58:28 +0200

    KA-132 - Create Service layer tests

---
+++ TodoList/Tests/TodoList.Api.Tests/Services/ListItemServicesTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using TodoList.Api.Tests.Api.Helpers;
+using TodoList.Contracts.Models;
+using TodoList.Contracts.Repositories;
+using TodoList.Contracts.Services;
+using TodoList.Services.ListItemController;
+
+namespace TodoList.Api.Tests.Services
+{
+    class ListItemServicesTest
+    {
+        private IListItemServices _listItemServices;
+        private IListItemGuidGenerator _guidGenerator;
+        private IListItemRepository _itemRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            _guidGenerator = Substitute.For<IListItemGuidGenerator>();
+            _itemRepository = Substitute.For<IListItemRepository>();
+
+            _listItemServices = new ListItemServices(_itemRepository, _guidGenerator);
+        }
+
+        [Test]
+        public void PostAsync_ReturnsCorrectListItem()
+        {
+            var itemGuid = new Guid("0478a8c4-4f17-49b1-b61b-df1156465505");
+            var postedItem = new ListItem() { Id = Guid.Empty, Text = "hippopotamus" };
+            var expectedItem = new ListItem() { Id = itemGuid, Text = "hippopotamus" };
+            _guidGenerator.GenerateGuid().Returns(itemGuid);
+            _itemRepository.CreateAsync(postedItem).Returns(expectedItem);
+
+            var actualItem = _listItemServices.PostAsync(postedItem).Result;
+            
+            Assert.That(actualItem, Is.EqualTo(expectedItem).UsingListItemComparer());
+        }
+    }
+}
