e80d77ddcc882bc676903bdf0fa49b022d5a118b
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-04 15:54:08 +0200

    KA-132 - Create IUpdateItemService

---
+++ TodoList/Sources/TodoList.Contracts/Services/IUpdateItemService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TodoList.Contracts.Models;
+
+namespace TodoList.Contracts.Services
+{
+    public interface IUpdateItemService
+    {
+        Task<ListItem> UpdateExistingItemAsync(ListItem item);
+    }
+}
