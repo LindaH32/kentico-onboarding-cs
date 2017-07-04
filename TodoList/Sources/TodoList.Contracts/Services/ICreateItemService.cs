23b40deccf0994a6bc230174d2eeeb4cba027c4f
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-04 15:50:22 +0200

    KA-132 - Create ICreateItemService

---
+++ TodoList/Sources/TodoList.Contracts/Services/ICreateItemService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TodoList.Contracts.Models;
+
+namespace TodoList.Contracts.Services
+{
+    public interface ICreateItemService
+    {
+        Task<ListItem> CreateNewItemAsync(ListItem item);
+    }
+}
