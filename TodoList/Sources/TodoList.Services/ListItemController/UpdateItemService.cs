ea5e73b482df5b56c5f30f1cd5d262b461f9a70e
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-04 16:03:05 +0200

    KA-132 - Create UpdateItemService

---
+++ TodoList/Sources/TodoList.Services/ListItemController/UpdateItemService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using TodoList.Contracts.Models;
+using TodoList.Contracts.Repositories;
+using TodoList.Contracts.Services;
+
+namespace TodoList.Services.ListItemController
+{
+    public class UpdateItemService : IUpdateItemService
+    {
+        private readonly IListItemRepository _itemRepository;
+        private readonly IDateTimeGenerator _dateTimeGenerator;
+
+        public UpdateItemService(IListItemRepository itemRepository, IDateTimeGenerator dateTimeGenerator)
+        {
+            _itemRepository = itemRepository;
+            _dateTimeGenerator = dateTimeGenerator;
+        }
+        public async Task<ListItem> UpdateExistingItemAsync(ListItem item)
+        {
+            //TODO
+            ListItem existingItem = await _itemRepository.GetAsync(item.Id);
+            existingItem.UpdateDateTime = _dateTimeGenerator.GenerateDateTime();
+            existingItem.Text = item.Text;
+
+            await _itemRepository.UpdateAsync(existingItem);
+            return existingItem;
+        }
+    }
+}
