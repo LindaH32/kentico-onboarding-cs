d619c743e750b94cc5ca07eee49b018f4a20e81b
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-07 15:29:40 +0200

    KA-132 - Move ServicesBootstrapper in root of Services Project

---
+++ TodoList/Sources/TodoList.Services/ServicesBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Unity;
+using TodoList.Contracts.Bootstrap;
+using TodoList.Contracts.Services;
+using TodoList.Services.ListItemController;
+
+namespace TodoList.Services.Bootstrap
+{
+    public class ServicesBootstrapper : IBootstrapper
+    {
+        public IUnityContainer Register(IUnityContainer container)
+            => container
+                .RegisterType<IGuidGenerator, GuidGenerator>(new HierarchicalLifetimeManager())
+                .RegisterType<ICreateItemService, CreateItemService>(new HierarchicalLifetimeManager())
+                .RegisterType<IUpdateItemService, UpdateItemService>(new HierarchicalLifetimeManager())
+                .RegisterType<IDateTimeGenerator, DateTimeGenerator>(new HierarchicalLifetimeManager());
+    }
+}
