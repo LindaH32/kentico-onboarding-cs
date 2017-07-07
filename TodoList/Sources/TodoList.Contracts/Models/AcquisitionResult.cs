630f6e2ce5f63be85510d89b309ce78d6fe9072e
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-07 15:15:08 +0200

    KA-132 - Create AcquisitionResult

---
+++ TodoList/Sources/TodoList.Contracts/Models/AcquisitionResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Dynamic;
+
+namespace TodoList.Contracts.Models
+{
+    public class AcquisitionResult
+    {
+        public static AcquisitionResult Create(ListItem item) 
+            => new AcquisitionResult {AcquiredItem = item};
+
+        private AcquisitionResult() { }
+
+        public ListItem AcquiredItem { get; set; }
+
+        public bool WasSuccessful => AcquiredItem != null;
+    }
+}
