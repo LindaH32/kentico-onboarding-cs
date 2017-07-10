5abecf1fc922dd85c34bbae9856cd8a5ac7785dd
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-10 13:04:36 +0200

    KA-132 - Add test in UpdateItemServiceTest

--- TodoList/Tests/TodoList.Services.Tests/Services/UpdateItemServiceTest.cs
+++ TodoList/Tests/TodoList.Services.Tests/Services/UpdateItemServiceTest.cs
@@ -5,7 +5,8 @@ using TodoList.Contracts.Base.Models;
 using TodoList.Contracts.Models;
 using TodoList.Contracts.Repositories;
 using TodoList.Contracts.Services;
-using TodoList.Services.ListItemController;
+using TodoList.Services.ListItemServices;
+using Assert = NUnit.Framework.Assert;
 
 namespace TodoList.Services.Tests.Services
 {
@@ -14,7 +15,7 @@ namespace TodoList.Services.Tests.Services
     {
         private IListItemRepository _itemRepository;
         private IDateTimeGenerator _dateTimeGenerator;
-        private IUpdateItemService _updateItemService;
+        private IItemModificationService _itemModificationService;
 
         [SetUp]
         public void Init()
@@ -22,24 +23,51 @@ namespace TodoList.Services.Tests.Services
             _itemRepository = Substitute.For<IListItemRepository>();
             _dateTimeGenerator = Substitute.For<IDateTimeGenerator>();
 
-            _updateItemService = new UpdateItemService(_itemRepository, _dateTimeGenerator);
+            _itemModificationService = new ItemModificationService(_itemRepository, _dateTimeGenerator);
         }
 
         [Test]
-        public void SetupAndUpdateItemAsync_ReturnsCorrectListItem()
+        public void SetupAndUpdateItemAsync_WithValidArguments_ReturnsCorrectListItem()
         {
             var itemGuid = new Guid("0478a8c4-4f17-49b1-b61b-df1156465505");
             var creationTime = new DateTime(year: 2017, month: 8, day: 1, hour: 12, minute: 55, second: 55);
-            var updateTime = new DateTime(year: 2017, month: 9, day: 2, hour: 13, minute: 56, second: 56);
-            var postedItem = new ListItem { Id = Guid.Empty, Text = "hippopotamus", CreationDateTime = creationTime, UpdateDateTime = creationTime };
-            var expectedItem = new ListItem { Id = itemGuid, Text = postedItem.Text, CreationDateTime = creationTime, UpdateDateTime = updateTime };
-            _itemRepository.GetAsync(Arg.Any<Guid>()).Returns(expectedItem); //TODO expectedItem is already set, doesnt test properly
-            _itemRepository.UpdateAsync(Arg.Any<ListItem>()).Returns(info => info.Arg<ListItem>());
+            var updateTime = new DateTime(year: 2018, month: 9, day: 2, hour: 13, minute: 56, second: 56);
+            var databaseListItem = new ListItem { Id = itemGuid, Text = "oldText", CreationDateTime = creationTime, UpdateDateTime = creationTime };
+            var postedListItem = new ListItem { Id = itemGuid, Text = "hippopotamus" };
+            var expectedListItem = new ListItem { Id = itemGuid, Text = postedListItem.Text, CreationDateTime = creationTime, UpdateDateTime = updateTime };
+            var acquisitionResult = AcquisitionResult.Create(databaseListItem);
             _dateTimeGenerator.GenerateDateTime().Returns(updateTime, DateTime.MinValue);
 
-            var actualItem = _updateItemService.UpdateExistingItemAsync(postedItem).Result;
+            var actualItem = _itemModificationService.UpdateExistingItemAsync(acquisitionResult, postedListItem).Result;
 
-            Assert.That(actualItem, Is.EqualTo(expectedItem).UsingListItemComparer());
+            Assert.That(actualItem, Is.EqualTo(expectedListItem).UsingListItemComparer());
         }
+
+        [Test]
+        public void SetupAndUpdateItemAsync_WithFailedAcquisitionResult_ThrowsCorrectException()
+        {
+            var itemGuid = new Guid("0478a8c4-4f17-49b1-b61b-df1156465505");
+            var postedListItem = new ListItem { Id = itemGuid, Text = "hippopotamus" };
+            var acquisitionResult = AcquisitionResult.Create(null);
+            //TODO fix aggregated exception throw
+            try
+            {
+                _itemModificationService.UpdateExistingItemAsync(acquisitionResult, postedListItem);
+            }
+            catch (AggregateException e)
+            {
+                if (e.InnerException.GetType() == typeof(ArgumentException))
+                {
+                    Assert.IsTrue(true);
+                }
+                else
+                {
+                    Assert.Fail("There is no ArgumentException m8");
+                }
+            }
+
+
+        }
+
     }
 }
