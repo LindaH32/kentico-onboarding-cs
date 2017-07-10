42137d0788fbf9076bd987553254668fce39993d
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-10 17:30:40 +0200

    KA-132 - Add tests to GuidGeneratorTest

--- TodoList/Tests/TodoList.Services.Tests/Wrappers/GuidGeneratorTest.cs
+++ TodoList/Tests/TodoList.Services.Tests/Wrappers/GuidGeneratorTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using TodoList.Contracts.Services;
 using TodoList.Services.Wrappers;
@@ -16,12 +18,27 @@ namespace TodoList.Services.Tests.Wrappers
         }
 
         [Test]
-        public void GenerateGuid_ReturnsCorrectGuid()
+        public async Task GenerateGuid_DoesntReturnDuplicateValues()
         {
-            var generatedGuid = _guidGenerator.GenerateGuid().ToString();
-            Guid guidOutput;
-            var isValid = Guid.TryParse(generatedGuid, out guidOutput);
-            Assert.IsTrue(isValid);
+            var numberOfIterations = 10;
+            List<Guid> generatedGuids = new List<Guid>();
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                generatedGuids.Add(await _guidGenerator.GenerateGuid());
+            }
+
+            Assert.That(generatedGuids, Is.Unique);
         }
+
+        [Test]
+        public async Task GenerateGuid_DoesntReturnEmptyValue()
+        {
+            var generatedGuid = await _guidGenerator.GenerateGuid();
+
+            Assert.That(generatedGuid, Is.Not.EqualTo(Guid.Empty));
+        }
+
+
     }
 }
