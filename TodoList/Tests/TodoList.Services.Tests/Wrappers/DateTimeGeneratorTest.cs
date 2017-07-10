5ed3ad2f9629ea51d1d277044a8bd3933dcedec7
Vlastimil Martinek <VlastimilM@kentico.com>
2017-07-10 14:52:48 +0200

    KA-132 - Create DateTimeGeneratorTest, GuidGeneratorTest

---
+++ TodoList/Tests/TodoList.Services.Tests/Wrappers/DateTimeGeneratorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using TodoList.Contracts.Services;
+using TodoList.Services.Wrappers;
+
+namespace TodoList.Services.Tests.Wrappers
+{
+    [TestFixture]
+    public class DateTimeGeneratorTest
+    {
+        private IDateTimeGenerator _dateTimeGenerator;
+
+        [SetUp]
+        public void Init()
+        {
+            _dateTimeGenerator = new DateTimeGenerator();
+        }
+
+        [Test]
+        public void GenerateDateTime_ReturnsCorrectDateTime()
+        {
+            Assert.That(_dateTimeGenerator.GenerateDateTime().Result, Is.EqualTo(DateTime.Now));
+        }
+        
+    }
+}
---
+++ TodoList/Tests/TodoList.Services.Tests/Wrappers/GuidGeneratorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using TodoList.Contracts.Services;
+using TodoList.Services.Wrappers;
+
+namespace TodoList.Services.Tests.Wrappers
+{
+    [TestFixture]
+    public class GuidGeneratorTest
+    {
+        private IGuidGenerator _guidGenerator;
+        [SetUp]
+        public void Init()
+        {
+            _guidGenerator = new GuidGenerator();
+        }
+
+        [Test]
+        public void GenerateGuid_ReturnsCorrectGuid()
+        {
+            var generatedGuid = _guidGenerator.GenerateGuid().ToString();
+            Guid guidOutput;
+            var isValid = Guid.TryParse(generatedGuid, out guidOutput);
+            Assert.IsTrue(isValid);
+        }
+    }
+}
