e2786787543cb0a9794a079998614b9214de1f08
Vlastimil Martinek <VlastimilM@kentico.com>
2017-08-02 10:34:13 +0200

    KA-133 - Remove duplicate tests

--- TodoList/Tests/TodoList.Api.Tests/Controllers/ListItemsControllerTest.cs
+++ TodoList/Tests/TodoList.Api.Tests/Controllers/ListItemsControllerTest.cs
@@ -89,20 +89,6 @@ namespace TodoList.Api.Tests.Controllers
         }
 
         [Test]
-        public void DeleteAsync_EmptyGuid_ReturnsCorrectErrorResponse()
-        {
-            var expectedKeys = new[] { "Id" };
-
-            var actionResult = _controller.DeleteAsync(Guid.Empty).Result;
-            var responseMessage = actionResult.ExecuteAsync(CancellationToken.None).Result;
-            HttpError error;
-            responseMessage.TryGetContentValue(out error);
-
-            Assert.That(responseMessage.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-            Assert.That(error.ModelState.Keys, Is.EqualTo(expectedKeys));
-        }
-
-        [Test]
         public void GetAsync_ById_ReturnsCorrectResponse()
         {
             var expectedListItem = new ListItem { Id = _guidOfFirstItem, Text = "text" };
@@ -271,7 +257,7 @@ namespace TodoList.Api.Tests.Controllers
         }
 
         [Test]
-        public void PutAsync_withEmptyGuid_ReturnsCorrectErrorResponse()
+        public void PutAsync_WithEmptyGuid_ReturnsCorrectErrorResponse()
         {
             var expectedKeys = new[] { "Id" };
             var editedListItem = new ListItem { Id = Guid.Empty, Text = "text" };
@@ -310,36 +296,6 @@ namespace TodoList.Api.Tests.Controllers
             var expectedKeys = new[] { string.Empty };
 
             var actionResult = _controller.PutAsync(null).Result;
-            var responseMessage = actionResult.ExecuteAsync(CancellationToken.None).Result;
-            HttpError error;
-            responseMessage.TryGetContentValue(out error);
-
-            Assert.That(responseMessage.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-            Assert.That(error.ModelState.Keys, Is.EqualTo(expectedKeys));
-        }
-
-        [Test]
-        public void PutAsync_ItemWithoutText_ReturnsCorrectErrorResponse()
-        {
-            var expectedKeys = new[] { "Text" };
-            var postedListItem = new ListItem { Id = _guidOfFirstItem, Text = null };
-
-            var actionResult = _controller.PutAsync(postedListItem).Result;
-            var responseMessage = actionResult.ExecuteAsync(CancellationToken.None).Result;
-            HttpError error;
-            responseMessage.TryGetContentValue(out error);
-
-            Assert.That(responseMessage.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-            Assert.That(error.ModelState.Keys, Is.EqualTo(expectedKeys));
-        }
-
-        [Test]
-        public void PutAsync_ItemWithEmptyGuid_ReturnsCorrectErrorResponse()
-        {
-            var expectedKeys = new[] { "Id" };
-            var itemWithEmptyGuid = new ListItem { Id = Guid.Empty, Text = "newText" };
-
-            var actionResult = _controller.PutAsync(itemWithEmptyGuid).Result;
             var responseMessage = actionResult.ExecuteAsync(CancellationToken.None).Result;
             HttpError error;
             responseMessage.TryGetContentValue(out error);
